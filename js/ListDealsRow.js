9e2aaf82d2a14f48ebec611e83fe96fc4fe53faa
robertogds <roberto@imaginasoft.com>
2011-05-26 16:00:53 +0200

    added icons

--- js/ListDealsRow.js
+++ js/ListDealsRow.js
@@ -2,7 +2,7 @@
   var ListDealsRow;
   ListDealsRow = (function() {
     function ListDealsRow(deal) {
-      var hotelLabel, image, normalPriceLabel, priceLabel, roomTypeLabel;
+      var hotelLabel, image, normalPriceLabel, priceLabel, roomTypeImage, tachaImage;
       this.row = Ti.UI.createTableViewRow({
         hasChild: true,
         identionLevel: 200,
@@ -30,61 +30,76 @@
         image: deal.mainImageSmall,
         borderRadius: 10,
         borderColor: '#fff',
-        borderWidth: 3,
+        borderWidth: 2,
         left: 4,
         width: 140,
         height: 110
       });
       this.row.add(image);
       hotelLabel = Titanium.UI.createLabel({
+        borderWidth: 0,
         text: deal.hotelName,
         color: '#fff',
         left: 155,
         font: {
-          fontSize: 14
+          fontSize: 14,
+          fontWeight: 'bold'
         },
-        height: 70,
-        width: 135,
-        top: 0
+        height: 30,
+        width: 160,
+        top: 5
       });
       this.row.add(hotelLabel);
       priceLabel = Titanium.UI.createLabel({
         text: deal.salePriceCents + "€",
         color: '#fff',
         left: 155,
+        borderWidth: 0,
         font: {
-          fontSize: 24,
+          fontSize: 28,
           fontWeight: 'bold'
         },
-        height: 70,
-        width: 135,
-        top: 25
+        height: 30,
+        width: 70,
+        top: 40
       });
       this.row.add(priceLabel);
       normalPriceLabel = Titanium.UI.createLabel({
         text: deal.priceCents + "€",
-        color: '#333',
-        left: 220,
+        color: '#bfbfbf',
+        left: 227,
+        borderWidth: 0,
+        textAlign: 'center',
         font: {
           fontSize: 14
         },
-        height: 70,
-        width: 135,
-        top: 25
+        height: 30,
+        width: 35,
+        top: 40
       });
       this.row.add(normalPriceLabel);
-      roomTypeLabel = Titanium.UI.createLabel({
-        text: deal.roomType,
-        color: '#fff',
-        left: 155,
-        font: {
-          fontSize: 14
-        },
-        height: 70,
-        width: 135,
-        top: 55
+      tachaImage = Titanium.UI.createImageView({
+        left: 226,
+        top: 55,
+        height: 1,
+        width: 35,
+        image: 'images/tacha.png'
       });
-      this.row.add(roomTypeLabel);
+      this.row.add(tachaImage);
+      roomTypeImage = Titanium.UI.createImageView({
+        left: 155,
+        top: 80,
+        height: 30,
+        width: 60
+      });
+      if (deal.roomType === 'Lujo') {
+        roomTypeImage.image = 'images/icon_lujo.png';
+      } else if (deal.roomType === 'Elegante') {
+        roomTypeImage.image = 'images/icon_elegante.png';
+      } else if (deal.roomType === 'Confort') {
+        roomTypeImage.image = 'images/icon_confort.png';
+      }
+      this.row.add(roomTypeImage);
     }
     return ListDealsRow;
   })();
--- src/ui/listDeals/ListDealsRow.coffee
+++ src/ui/listDeals/ListDealsRow.coffee
@@ -6,62 +6,78 @@ class ListDealsRow
       height: 120
       backgroundGradient:
         type:'linear'
-        #colors:[{color:'#494949',position:0.1},{color:'#3f3f3f',position:0.50},{color:'#2b2b2b',position:1.0}]
         colors:[{color:'#093344',position:0.1},{color:'#0b222e',position:0.50},{color:'#0c1b24',position:1.0}]
       fontSize: 6
       deal: deal
       color: '#fff'
+
     image = Titanium.UI.createImageView 
       image: deal.mainImageSmall
       borderRadius: 10
       borderColor: '#fff'
-      borderWidth: 3
+      borderWidth: 2
       left: 4
       width: 140
       height: 110
     @row.add(image)
+
     hotelLabel = Titanium.UI.createLabel
+      borderWidth: 0
       text: deal.hotelName
       color: '#fff'
       left: 155
       font:
         fontSize: 14
-      height: 70
-      width: 135
-      top: 0
+        fontWeight: 'bold'
+      height: 30
+      width: 160
+      top: 5
     @row.add(hotelLabel)
     priceLabel = Titanium.UI.createLabel
       text: deal.salePriceCents + "€"
       color: '#fff'
       left: 155
+      borderWidth: 0
       font:
-        fontSize: 24
+        fontSize: 28
         fontWeight: 'bold'
-      height: 70
-      width: 135
-      top: 25
+      height: 30
+      width: 70
+      top: 40
     @row.add(priceLabel)
 
     normalPriceLabel = Titanium.UI.createLabel
       text: deal.priceCents + "€"
-      color: '#333'
-      left: 220
+      color: '#bfbfbf'
+      left: 227
+      borderWidth: 0
+      textAlign: 'center'
       font:
         fontSize: 14
-      height: 70
-      width: 135
-      top: 25
+      height: 30
+      width: 35
+      top: 40
     @row.add(normalPriceLabel)
-
-    roomTypeLabel = Titanium.UI.createLabel
-      text: deal.roomType
-      color: '#fff'
-      left: 155
-      font:
-        fontSize: 14
-      height: 70
-      width: 135
+   
+    tachaImage = Titanium.UI.createImageView 
+      left: 226
       top: 55
-    @row.add(roomTypeLabel)
+      height: 1
+      width: 35
+      image: 'images/tacha.png'
+    @row.add(tachaImage)
+
+    roomTypeImage = Titanium.UI.createImageView 
+      left: 155
+      top: 80
+      height: 30
+      width: 60
+    if deal.roomType is 'Lujo'
+      roomTypeImage.image = 'images/icon_lujo.png'
+    else if deal.roomType is 'Elegante'
+      roomTypeImage.image = 'images/icon_elegante.png'
+   	else if deal.roomType is 'Confort'
+	      roomTypeImage.image = 'images/icon_confort.png'
+    @row.add(roomTypeImage)
 
 root.listDealsRow = ListDealsRow
